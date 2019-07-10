fd53e01210e469772167b955fd5d3b8226dc5c51
Hanul <hanul@hanul.me>
2019-07-10 18:54:01 +0900

    좌표를 입력하지 않아도 되도록

--- SkyEngine/BROWSER/Node/Figure/Line.js
+++ SkyEngine/BROWSER/Node/Figure/Line.js
@@ -314,10 +314,10 @@ SkyEngine.Line = CLASS((cls) => {
 	
 		init : (inner, self, params) => {
 			//REQUIRED: params
-			//REQUIRED: params.startX		직선의 시작 x 좌표
-			//REQUIRED: params.startY		직선의 시작 y 좌표
-			//REQUIRED: params.endX			직선의 끝 x 좌표
-			//REQUIRED: params.endY			직선의 끝 Y 좌표
+			//OPTIONAL: params.startX		직선의 시작 x 좌표
+			//OPTIONAL: params.startY		직선의 시작 y 좌표
+			//OPTIONAL: params.endX			직선의 끝 x 좌표
+			//OPTIONAL: params.endY			직선의 끝 Y 좌표
 			//REQUIRED: params.border
 			//OPTIONAL: params.isEndless	true로 지정하면 양 끝이 무한인 직선을 생성합니다.
 			
@@ -327,6 +327,19 @@ SkyEngine.Line = CLASS((cls) => {
 			let endY = params.endY;
 			let border = params.border;
 			let isEndless = params.isEndless;
+			
+			if (startX === undefined) {
+				startX = 0;
+			}
+			if (startY === undefined) {
+				startY = 0;
+			}
+			if (endX === undefined) {
+				endX = 0;
+			}
+			if (endY === undefined) {
+				endY = 0;
+			}
 			
 			let checkLineLine = SkyEngine.Util.Collision.checkLineLine;
 			let checkLineRect = SkyEngine.Util.Collision.checkLineRect;
